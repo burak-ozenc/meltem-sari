8ff7d7230e3c56da15dff0b9a19ee01259cee6af
brk.ozenc <brk.ozenc@gmail.com>
2025-12-27 21:54:20 +0100

    vercel doesnt like envs - projectId added

--- src/lib/sanity.js
+++ src/lib/sanity.js
@@ -2,8 +2,8 @@ import { createClient } from '@sanity/client'
 import imageUrlBuilder from '@sanity/image-url'
 
 export const client = createClient({
-  projectId: process.env.VITE_SANITY_PROJECT_ID,
-  dataset: process.env.VITE_SANITY_DATASET,
+    projectId: process.env.VITE_SANITY_PROJECT_ID || 'jl0heffc',
+    dataset: process.env.VITE_SANITY_DATASET || 'production',
   useCdn: false,
   apiVersion: '2024-01-01',
 })
