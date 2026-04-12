06e62df41a947a6c545369f493a87c9ac3fbf351
samiboran <78107283+samiboran@users.noreply.github.com>
2026-04-12 13:56:32 +0300

    Update Exhibitions.jsx

--- src/pages/Exhibitions.jsx
+++ src/pages/Exhibitions.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react'
-import { client } from '../lib/sanity'
+import { client, urlFor } from '../lib/sanity'
 import './Exhibitions.css'
 
 const Exhibitions = () => {
@@ -16,7 +16,8 @@ const Exhibitions = () => {
           type,
           venue,
           location,
-          description
+          description,
+          images
         }`
         
         const data = await client.fetch(query)
@@ -48,27 +49,4 @@ const Exhibitions = () => {
             className="exhibition-item"
             style={{ animationDelay: `${index * 0.1}s` }}
           >
-            <div className="exhibition-year">{exhibition.year}</div>
-            <div className="exhibition-details">
-              <h3 className="exhibition-title">
-                {exhibition.venue}
-                {exhibition.location && `, ${exhibition.location}`}
-              </h3>
-              <p className="exhibition-type">
-                {exhibition.type === 'solo' && 'Solo Exhibition'}
-                {exhibition.type === 'group' && 'Group Exhibition'}
-                {exhibition.type === 'installation' && 'Installation'}
-                {exhibition.title && ` / ${exhibition.title}`}
-              </p>
-              {exhibition.description && (
-                <p className="exhibition-description">{exhibition.description}</p>
-              )}
-            </div>
-          </div>
-        ))}
-      </div>
-    </div>
-  )
-}
-
-export default Exhibitions
+            <div className="exhibition-year">{exhibition.
